02ae343586d749d43963f87db9ffbf98b5f622d9
waki285 <suzuneu@suzuneu.com>
2021-12-08 05:41:56 +0000

    ✨ feat: sleep

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const { Worker } = require("worker_threads");
 const puppeteer = require("puppeteer");
 
+const sleep = async (ms) => {
+  return new Promise(r => setTimeout(r, ms));
+}
+
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
@@ -9,6 +13,8 @@ const puppeteer = require("puppeteer");
   
   const page = await browser.newPage();
   await page.goto("chrome://extensions");
+
+  await sleep(1000);
 
   await page.click('cr-toggle[aria-labelledby="devModeLabel"]')
 
