dc76616e57944f797d2da575069d7e575cb785ef
waki285 <suzuneu@suzuneu.com>
2021-12-08 05:40:40 +0000

    ✨ feat: click

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const puppeteer = require("puppeteer");
   });
   
   const page = await browser.newPage();
-  await page.goto("https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo");
+  await page.goto("chrome://extensions");
 
-  await page.click('div[class^="dd-Va g-c-wb g-eg-ua-Uc-c-za g-c-Oc-td-jb-oa"]')
+  await page.click('cr-toggle[aria-labelledby="devModeLabel"]')
 
 })();
